df541e38a2c321de3a838e7ef5fe2fe58723c134
Adil <adil0@list.ru>
2019-10-31 12:37:49 +0600

    Init

---
+++ index.js
@@ -0,0 +1,9 @@
+function resize() {
+  var content=document.querySelector(".content")
+  var main=document.querySelector(".main-content")
+  main.style.width='63%'
+  main.style.borderRight='1px solid rgba(0,0,0,0.2)'
+  var edit=document.querySelector(".edit")
+  edit.style.display='block'
+
+}
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="index.css">
+    <title></title>
+  </head>
+  <body>
+    <section class='nav'>
+      <nav>
+
+      </nav>
+    </section>
+    <section class="content">
+        <section class="tabs">
+
+        </section>
+        <section class="main-content">
+          <button type="button" name="button" onclick="resize()" >open</button>
+        </section>
+        <div class="edit">
+
+        </div>
+
+    </section>
+    <script type="text/javascript" src='index.js'>
+
+    </script>
+  </body>
+</html>
