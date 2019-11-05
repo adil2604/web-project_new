443f1461bcc33624a8604fff996a97bccbb566a6
Adil <adil0@list.ru>
2019-11-06 00:54:14 +0600

    some improvements

--- index.js
+++ index.js
@@ -7,3 +7,88 @@ function resize() {
   edit.style.display='block'
 
 }
+
+function setClass(div, className) {
+  for(let j=0;j<div.length;j++){
+    div[j].className=className
+  }
+}
+
+
+
+var btns=document.querySelectorAll(".tabs-box button")
+
+
+
+function setActive(i) {
+let content=document.querySelectorAll(".index")
+setClass(content,'index');
+content[i].className='index showing'
+setClass(btns,'');
+btns[i].className='active'
+}
+
+
+
+
+function makeRequest(url) {
+        var httpRequest = false;
+
+        if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+            httpRequest = new XMLHttpRequest();
+            if (httpRequest.overrideMimeType) {
+                httpRequest.overrideMimeType('text/xml');
+                // Читайте ниже об этой строке
+            }
+        } else if (window.ActiveXObject) { // IE
+            try {
+                httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                try {
+                    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {}
+            }
+        }
+
+        if (!httpRequest) {
+            alert('Не вышло :( Невозможно создать экземпляр класса XMLHTTP ');
+            return false;
+        }
+        httpRequest.onreadystatechange = function() { alertContents(httpRequest); };
+        httpRequest.open('GET', url, true);
+        httpRequest.send(null);
+
+    }
+
+    function alertContents(httpRequest) {
+
+        if (httpRequest.readyState == 4) {
+            if (httpRequest.status == 200) {
+                alert(httpRequest.responseText);
+            } else {
+                alert('С запросом возникла проблема.');
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+var tasks=document.querySelectorAll(".tasks .addTask")
+for (var i = 0; i < tasks.length; i++) {
+  tasks[i].addEventListener('keydown',function (e) {
+    var keyCode = e.keyCode || e.which;
+    if ( keyCode === 13 ) {
+      alert(this.value)
+      var btn=document.createElement('button')
+      btn.appendChild(document.createTextNode(this.value))
+      this.text= '';
+      // this.removeChild(this.childNodes[1])
+
+    }
+  })
+}
--- index.php
+++ index.php
@@ -3,20 +3,99 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="index.css">
+    <link rel="icon" href="asserts/icons/icon.png">
     <title></title>
   </head>
   <body>
     <section class='nav'>
-      <nav>
-
+      <nav style="width: 100%;height: 100%">
+        <img src="asserts/icons/icon.png" alt="Logo" style="width:auto;height: 100%; margin-left: 2vw;">
+        <form class="search" action="" method="post" style="width: 30%;height: 50%;display:flex;" >
+          <input type="text" name="search-input" class="search-input" value="" placeholder="Search in tasks....">
+          <input type="button" name=""  id="search"value="" style="border:0; width: 10%;height: 108%; margin:0">
+        </form>
+        <a href="" class="profile" style="height: 100%;width: 5%;margin-right: 2vw;"></a>
       </nav>
     </section>
-    <section class="content">
+    <section class="content" >
         <section class="tabs">
+          <div class="tabs-box" style="width:100%;height:70%;margin:3vw 0 auto auto;display:flex;flex-direction:column;">
+            <button type="button" class="active" name="button" style="background-image: url('asserts/icons/0.png');" onclick="setActive(0)">Important</button>
+            <button type="button" name="button" style="background-image: url('asserts/icons/1.png');" onclick="setActive(1)">Today tasks</button>
+            <button type="button" name="button" style="background-image: url('asserts/icons/2.png');" onclick="setActive(2)">Planned tasks</button>
 
+
+
+          </div>
         </section>
         <section class="main-content">
-          <button type="button" name="button" onclick="resize()" >open</button>
+          <div id="important" class="index showing" >
+            <div class="info">
+              <div class="">
+                <h2>Important</h2>
+              </div>
+            </div>
+            <div class="tasks">
+              <div class="task">
+                    <input type="text" class="addTask" name=""  onsubmit="makeRequest('test.php')" value="">
+              </div>
+              <div class="task">
+                <input type="text" class="addTask" name="" value="">
+              </div>
+              <div class="task">
+                <input type="text" class="addTask" name="" value="">
+              </div>
+              <div class="task">
+                <input type="text" class="addTask" name="" value="">
+              </div>
+
+            </div>
+          </div>
+          <div id="today" class="index">
+            <div class="info">
+              <div class="">
+                <h2>Today tasks</h2>
+              </div>
+            </div>
+            <div class="tasks">
+              <div class="task">
+                <input type="text" class="addTask" name="" value="">
+              </div>
+              <div class="task">
+                <input type="text" class="addTask" name="" value="">
+              </div>
+              <div class="task">
+                <input type="text" class="addTask" name="" value="">
+              </div>
+              <div class="task">
+                <input type="text" class="addTask" name="" value="">
+              </div>
+
+            </div>
+            <button type="button" name="button" onclick="resize()" >open</button>
+          </div>
+          <div id="planned" class="index">
+            <div class="info">
+              <div class="">
+                <h2>Planned tasks</h2>
+              </div>
+            </div>
+            <div class="tasks">
+              <div class="task">
+                <input type="text" class="addTask" name="" value="">
+              </div>
+              <div class="task">
+                <input type="text" class="addTask" name="" value="">
+              </div>
+              <div class="task">
+                <input type="text" class="addTask" name="" value="">
+              </div>
+              <div class="task">
+                <input type="text" class="addTask" name="" value="">
+              </div>
+
+            </div>
+          </div>
         </section>
         <div class="edit">
 
